23dc634aa7c937af2c574aba50ccfb56bce32a5f
Diego Frata <diego.frata@gmail.com>
2012-12-20 16:00:09 -0200

    NuGet package version bumped to 1.0.2.

--- pack-nuget.bat
+++ pack-nuget.bat
@@ -1,3 +1,3 @@
 ..\NuGet.exe pack "RdioNet\RdioNet.csproj" -Build -BasePath "RdioNet\bin\Release" -OutputDirectory "RdioNet\bin\Release" -Properties Configuration=Release;Platform=AnyCPU
-..\NuGet.exe push "RdioNet\bin\Release\RdioNet.1.0.1.0.nupkg"
+..\NuGet.exe push "RdioNet\bin\Release\RdioNet.1.0.2.nupkg"
 PAUSE
